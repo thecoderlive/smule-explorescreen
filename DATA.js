39beccc5360e8d0a0405362be7be9472450f13b3
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-07 15:58:10 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,82 @@
+export const item = {
+    "id": "b03d92c7-6c1d-42fb-8177-61107cde29e2",
+    "currently_live_user": [
+        {
+            "id": "f96ff851-0caa-4dd2-94f4-56265b33630b",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/257.png",
+            "song_name": "provide",
+            "user_name": "middle",
+            "views_count": 2847
+        },
+        {
+            "id": "775b38f4-6342-4cde-9392-fde3bafb43ae",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/133.png",
+            "song_name": "guy",
+            "user_name": "mention",
+            "views_count": 8443
+        },
+        {
+            "id": "438a134b-9fa4-4244-91e4-5ace55fdeee5",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/104.png",
+            "song_name": "lay",
+            "user_name": "kitchen",
+            "views_count": 2158
+        },
+        {
+            "id": "1b1691a5-6e2c-4fc7-ae4a-79af9720811d",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/132.png",
+            "song_name": "east",
+            "user_name": "option",
+            "views_count": 4901
+        },
+        {
+            "id": "f8f4469d-ab21-4e26-97f1-f3a61f6fb466",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/197.png",
+            "song_name": "message",
+            "user_name": "drive",
+            "views_count": 1997
+        }
+    ],
+    "recommended_song_list": [
+        {
+            "id": "1f8196a2-ee5b-44a9-8c56-3838cd98d17b",
+            "recommended_song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/328.png",
+            "recommended_song_name": "house",
+            "recommended_user": "production",
+            "song_views": 3998,
+            "like_count": 9816
+        },
+        {
+            "id": "364c8cd2-b0d8-4438-ba05-4eb46b921850",
+            "recommended_song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/155.png",
+            "recommended_song_name": "fund",
+            "recommended_user": "song",
+            "song_views": 3695,
+            "like_count": 8388
+        },
+        {
+            "id": "71c370ac-abfe-4280-94eb-a56fa1508312",
+            "recommended_song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/331.png",
+            "recommended_song_name": "simple",
+            "recommended_user": "already",
+            "song_views": 5998,
+            "like_count": 458
+        },
+        {
+            "id": "79fa00df-189c-45da-b54a-76403b6493f2",
+            "recommended_song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/217.png",
+            "recommended_song_name": "society",
+            "recommended_user": "fall",
+            "song_views": 2712,
+            "like_count": 69
+        },
+        {
+            "id": "5ea87a37-596e-4955-98ee-067a807bfad3",
+            "recommended_song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/267.png",
+            "recommended_song_name": "down",
+            "recommended_user": "beautiful",
+            "song_views": 7789,
+            "like_count": 6197
+        }
+    ]
+}
